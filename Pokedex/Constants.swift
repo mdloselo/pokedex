442d5344ed4aa8dfa0549c5590dda9af85b89a45
mdloselo <mdloselo@gmail.com>
2017-01-15 20:17:47 +0200

    first commit

---
+++ Pokedex/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Pokedex
+//
+//  Created by Mohau Loselo on 2017/01/15.
+//  Copyright © 2017 Mohau Loselo. All rights reserved.
+//
+
+import Foundation
+
+typealias DownloadCompleted = () -> ()
+typealias JSONDictionary = Dictionary<String, AnyObject>
---
+++ Pokedex/MainVC.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Pokedex
+//
+//  Created by Mohau Loselo on 2017/01/15.
+//  Copyright © 2017 Mohau Loselo. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+class MainVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
